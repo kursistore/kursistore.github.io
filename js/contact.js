bc3a4bb55259f87bc93c4cbd68c819cab0faa940
Anant <95anantsingh@gmail.com>
2021-05-17 00:38:49 +0530

    major update

--- js/contact.js
+++ js/contact.js
@@ -1,31 +1,57 @@
+if (window.screen.width > 800) {
+  document.getElementById("buttonCallNow").innerText = "Request Callback";
+}
 
-const emailformat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+function validateContactForm() {
+  var name = document.getElementsByName("CFname")[0];
+  var email = document.getElementsByName("CFemail")[0];
+  var message = document.getElementsByName("CFmessage")[0];
 
-function validateForm() {
-  var email = document.getElementsByClassName("form-control")[0].value;
-  // if (email == "" || !emailformat.test(String(email).toLowerCase())) {
-  //   document.getElementById("submitsucess").innerText =
-  //     "Please enter a valid email.";
-  // } else {
-  //   submitContactForm();
-  // }
+  const emailformat =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  if (email == "") {
-    document.getElementById("submitsucess").innerText =
-      "Please enter an Email or Phone Number.";
+  if (name.value == "") {
+    name.classList.add("error");
+    name.value = "";
+    name.placeholder = "Please enter your Name*";
+    name.focus();
+  } else if (
+    email.value == "" ||
+    !emailformat.test(String(email.value).toLowerCase())
+  ) {
+    email.classList.add("error");
+    email.value = "";
+    email.placeholder = "Please enter a valid Email*";
+    email.focus();
+  } else if (message.value == "") {
+    message.classList.add("error");
+    message.value = "";
+    message.placeholder = "Please enter your Message*";
+    message.focus();
   } else {
     submitContactForm();
   }
 }
 
 function submitContactForm() {
-  document.getElementById("CEmail").value = document.getElementsByClassName(
-    "form-control"
-  )[0].value;
+  document.getElementById("CFname").value =
+    document.getElementsByName("CFname")[0].value;
+  document.getElementById("CFemail").value =
+    document.getElementsByName("CFemail")[0].value;
+  document.getElementById("CFsubject").value =
+    document.getElementsByName("CFsubject")[0].value;
+  document.getElementById("CFmessage").value =
+    document.getElementsByName("CFmessage")[0].value;
+
   document.getElementsByName("ContactForm")[0].submit();
-  document.getElementById("request-container").style.display = "none";
-  document.getElementById("submitsucess").innerText =
-    "Thankyou, we will contact you shortly.";
+  document.getElementsByClassName("contact-section")[0].style.display = "none";
+  // document.getElementById("submitsucess").innerText =
+  //   "Thankyou, we will contact you shortly.";
 }
-
-
+function scrolltoFooter() {
+  if (window.screen.width > 800) {
+    document.getElementsByName("EMAIL")[0].focus();
+  } else {
+    window.open('tel:+917669542408');
+  }
+}
--- js/footer.js
+++ js/footer.js
@@ -31,3 +31,34 @@ function doAfterFooterLoad() {
   document.getElementById("copywriteText").innerHTML =
     "Copyright &copy " + new Date().getFullYear();
 }
+
+const emailformat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+function validateConnectForm() {
+  var email = document.getElementsByClassName("form-control")[0].value;
+  // if (email == "" || !emailformat.test(String(email).toLowerCase())) {
+  //   document.getElementById("submitsucess").innerText =
+  //     "Please enter a valid email.";
+  // } else {
+  //   submitContactForm();
+  // }
+
+  if (email == "") {
+    document.getElementById("submitsucess").innerText =
+      "Please enter an Email or Phone Number.";
+  } else {
+    submitConnectForm();
+  }
+}
+
+function submitConnectForm() {
+  document.getElementById("CEmail").value = document.getElementsByClassName(
+    "form-control"
+  )[0].value;
+  document.getElementsByName("ConnectForm")[0].submit();
+  document.getElementById("request-container").style.display = "none";
+  document.getElementById("submitsucess").innerText =
+    "Thankyou, we will contact you shortly.";
+}
+
+
